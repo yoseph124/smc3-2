2c0136a61ce1a84ca69923cf9882eb74437ad505
BUGM <taegun0306@naver.com>
2021-04-01 15:33:42 +0900

    0401프로잭트 생성

---
+++ 0401/src/Prob_01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Prob_01 {
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+		Grade me=new Grade();
+		me.math=s.nextInt();
+		me.science=s.nextInt();
+		me.english=s.nextInt();
+		System.out.println("평균은"+me.average());
+	}
+
+}
+
+class Grade{
+	int math,science,english;
+	public int average(){
+		return (math+science+english)/3;
+	}
+}
